0f45028f05087a29f832d2cfeb3e983a609c0ceb
spragul <spragul33@gmail.com>
2023-05-23 16:26:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -20,55 +20,55 @@ import PaymentApp from './cart/payment';
 function App() {
   return (
     <div className="App">
-      <Route exact path="/login">
-        <Login />
-      </Route>
       <Route path="/">
-        <ListOfProduct />
+        <ListOfProduct/>
       </Route>
-      <Route path="/cart/create/:productId/:index">
-        <Cart />
-      </Route>
-      <Route path="/detail/cart">
-        <RentalCart />
-      </Route>
-      <Route path="/rentalpay/payment/:index">
-        <PaymentApp />
-      </Route>
+        <Route path="/cart/create/:productId/:index">
+          <Cart/>
+        </Route>
+        <Route path="/detail/cart">
+          <RentalCart/>
+        </Route>
+        <Route path="/rentalpay/payment/:index">
+          <PaymentApp/>
+        </Route>
 
-      <Route path="/product/detail/:id">
-        <ProductDetails />
-      </Route  >
+        <Route path="/product/detail/:id">
+          <ProductDetails/>
+        </Route  >
+          
+        <Route path="/add/product">
+          <AddProduct/>
+        </Route>
 
-      <Route path="/add/product">
-        <AddProduct />
-      </Route>
+        <Route path="/edit/product/:id">
+          <EditProducts/>
+        </Route>
+        <Route path="/login">
+        <Login/>
+        </Route>
+        <Route path="/signup">
+         <Signup/>
+        </Route>
+        <Route path="/forgotpassword">
+         <Forgot/>
+        </Route>
+        <Route path="/resetpassword">
+         <Reset/>
+        </Route>
+        
+        <Route path="/admin">
+          <DarkExample/>
+        </Route>
 
-      <Route path="/edit/product/:id">
-        <EditProducts />
-      </Route>
-      <Route path="/signup">
-        <Signup />
-      </Route>
-      <Route path="/forgotpassword">
-        <Forgot />
-      </Route>
-      <Route path="/resetpassword">
-        <Reset />
-      </Route>
+        <Route path="/issued/product/:id">
+          <ProductIssued/>
+        </Route>
+        <Route path="/product/issued">
+          <ListIssuedProduct/>
+        </Route>
 
-      <Route path="/admin">
-        <DarkExample />
-      </Route>
-
-      <Route path="/issued/product/:id">
-        <ProductIssued />
-      </Route>
-      <Route path="/product/issued">
-        <ListIssuedProduct />
-      </Route>
-
-
+  
     </div>
   );
 }
